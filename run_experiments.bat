87615c3a218b9912a12ab86be1c175ce23fdac71
pedrocg42 <pedrocorglez@gmail.com>
2022-12-06 05:57:51 +0100

    feat: updating batch and bash scripts

--- run_experiments.bat
+++ run_experiments.bat
@@ -1,3 +1,6 @@
+python train.py --experiment baseline_scenario_1
+python evaluate.py --experiment baseline_scenario_1
+
 @REM #################################################################
 @REM ################            SCENARIO 1              #############
 @REM #################################################################
@@ -8,26 +11,90 @@
 @REM python train.py --experiment vq_scenario_1
 @REM python evaluate.py --experiment vq_scenario_1
 
-python train.py --experiment dkvb_scenario_1
-python evaluate.py --experiment dkvb_scenario_1
+@REM python train.py --experiment dkvb_scenario_1
+@REM python evaluate.py --experiment dkvb_scenario_1
 
-python train.py --experiment vq_scenario_1_64
-python evaluate.py --experiment vq_scenario_1_64
+@REM python train.py --experiment vq_scenario_1_64
+@REM python evaluate.py --experiment vq_scenario_1_64
 
-python train.py --experiment dkvb_scenario_1_64
-python evaluate.py --experiment dkvb_scenario_1_64
+@REM python train.py --experiment dkvb_scenario_1_64
+@REM python evaluate.py --experiment dkvb_scenario_1_64
 
-python train.py --experiment vq_scenario_1_128
-python evaluate.py --experiment vq_scenario_1_128
+@REM python train.py --experiment vq_scenario_1_128
+@REM python evaluate.py --experiment vq_scenario_1_128
 
-python train.py --experiment dkvb_scenario_1_128
-python evaluate.py --experiment dkvb_scenario_1_128
+@REM python train.py --experiment dkvb_scenario_1_128
+@REM python evaluate.py --experiment dkvb_scenario_1_128
 
-python train.py --experiment vq_scenario_1_512
-python evaluate.py --experiment vq_scenario_1_512
+@REM python train.py --experiment vq_scenario_1_512
+@REM python evaluate.py --experiment vq_scenario_1_512
 
-python train.py --experiment dkvb_scenario_1_512
-python evaluate.py --experiment dkvb_scenario_1_512
+@REM python train.py --experiment dkvb_scenario_1_512
+@REM python evaluate.py --experiment dkvb_scenario_1_512
+
+
+@REM #################################################################
+@REM ################            SCENARIO 2              #############
+@REM #################################################################
+
+@REM python train.py --experiment baseline_scenario_2
+@REM python evaluate.py --experiment baseline_scenario_2
+
+@REM python train.py --experiment vq_scenario_2
+@REM python evaluate.py --experiment vq_scenario_2
+
+@REM python train.py --experiment dkvb_scenario_2
+@REM python evaluate.py --experiment dkvb_scenario_2
+
+@REM python train.py --experiment vq_scenario_2_64
+@REM python evaluate.py --experiment vq_scenario_2_64
+
+@REM python train.py --experiment dkvb_scenario_2_64
+@REM python evaluate.py --experiment dkvb_scenario_2_64
+
+@REM python train.py --experiment vq_scenario_2_128
+@REM python evaluate.py --experiment vq_scenario_2_128
+
+@REM python train.py --experiment dkvb_scenario_2_128
+@REM python evaluate.py --experiment dkvb_scenario_2_128
+
+@REM python train.py --experiment vq_scenario_2_512
+@REM python evaluate.py --experiment vq_scenario_2_512
+
+@REM python train.py --experiment dkvb_scenario_2_512
+@REM python evaluate.py --experiment dkvb_scenario_2_512
+
+
+@REM #################################################################
+@REM ################            SCENARIO 3              #############
+@REM #################################################################
+
+@REM python train.py --experiment baseline_scenario_3
+@REM python evaluate.py --experiment baseline_scenario_3
+
+@REM python train.py --experiment vq_scenario_3
+@REM python evaluate.py --experiment vq_scenario_3
+
+@REM python train.py --experiment dkvb_scenario_3
+@REM python evaluate.py --experiment dkvb_scenario_3
+
+@REM python train.py --experiment vq_scenario_3_64
+@REM python evaluate.py --experiment vq_scenario_3_64
+
+@REM python train.py --experiment dkvb_scenario_3_64
+@REM python evaluate.py --experiment dkvb_scenario_3_64
+
+@REM python train.py --experiment vq_scenario_3_128
+@REM python evaluate.py --experiment vq_scenario_3_128
+
+@REM python train.py --experiment dkvb_scenario_3_128
+@REM python evaluate.py --experiment dkvb_scenario_3_128
+
+@REM python train.py --experiment vq_scenario_3_512
+@REM python evaluate.py --experiment vq_scenario_3_512
+
+@REM python train.py --experiment dkvb_scenario_3_512
+@REM python evaluate.py --experiment dkvb_scenario_3_512
 
 
 @REM #################################################################
