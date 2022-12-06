2bf5243221790ff6aeca6235134e95ea0a6000f4
pedrocg42 <pedrocorglez@gmail.com>
2022-12-06 05:56:29 +0100

    feat: updating evaluate utils

--- my_utils/evaluate.py
+++ my_utils/evaluate.py
@@ -1,6 +1,9 @@
 from typing import Tuple
 
+import config
+import imagesize
 import numpy as np
+from PIL import Image
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, Dataset
@@ -8,105 +11,62 @@ from torchmetrics import F1Score, MeanSquaredError
 from tqdm import tqdm
 
 
+@torch.no_grad()
 def evaluate_dataset_patchwise(
     model: nn.Module,
-    dataset: Dataset,
     data_loader: DataLoader,
     crop_size: Tuple[int] = (256, 256),
-    device: str = "cuda",
 ):
 
     # Initializing metrics
-    f1 = F1Score(num_classes=1)
+    f1 = F1Score(num_classes=1, task="binary")
     mse = MeanSquaredError()
 
-    # Calculating number of patches per image
-    num_patches = [
-        np.ceil(height / crop_size[0]).astype(int) * np.ceil(width / crop_size[1]).astype(int)
-        for width, height in dataset.images_sizes
-    ]
+    pbar = tqdm(data_loader)
 
-    with torch.no_grad():
+    tp = 0
+    fp = 0
+    fn = 0
+    mse = 0.0
+    num_pixels = 0
+    for i, (patches, label) in enumerate(pbar):
 
-        image_index = 0
-        label_queue_patches = torch.empty((0, 1, *crop_size))
-        output_queue_patches = torch.empty((0, 1, *crop_size))
-        pbar = tqdm(data_loader)
+        patches = patches[0].to(config.device)
+        label = label[0]
 
-        tp = 0
-        tn = 0
-        fp = 0
-        fn = 0
-        mse = 0.0
-        num_pixels = 0
-        for i, batch in enumerate(pbar):
+        # Inference
+        output_patches = model(patches)
 
-            images, labels = batch
+        height, width = label.shape[-2:]
 
-            images = images.to(device)
-            labels = labels.to(device)
+        output_image = torch.zeros((1, height, width), dtype=torch.float32)
 
-            # Inference
-            output = model(images)
+        # Calculating necessary patches
+        num_patches_height = np.ceil(height / crop_size[0]).astype(int)
+        num_patches_width = np.ceil(width / crop_size[1]).astype(int)
 
-            # Concatenatig new patches to queue
-            label_queue_patches = torch.cat((label_queue_patches, labels.cpu()))
-            output_queue_patches = torch.cat((output_queue_patches, output.cpu()))
+        k = 0
+        for i in range(num_patches_height):
+            for j in range(num_patches_width):
 
-            # Checking if there's enough patches to form an image
-            if label_queue_patches.shape[0] >= num_patches[image_index]:
-                # Ensambling as many images as the patches extracted allow us
-                while label_queue_patches.shape[0] >= num_patches[image_index]:
+                top = i * crop_size[0]
+                left = j * crop_size[1]
 
-                    width, height = dataset.images_sizes[image_index]
+                # Avoiding the patch to be outside the image
+                if top + crop_size[0] >= height:
+                    top = height - crop_size[0]
+                if left + crop_size[1] >= width:
+                    left = width - crop_size[1]
 
-                    output_image = torch.zeros((1, height, width), dtype=torch.float32)
-                    label_image = torch.zeros((1, height, width), dtype=torch.float32)
+                output_image[:, top : top + crop_size[0], left : left + crop_size[1]] = output_patches[k]
+                k += 1
 
-                    # Calculating necessary patches
-                    num_patches_height = np.ceil(height / crop_size[0]).astype(int)
-                    num_patches_width = np.ceil(width / crop_size[1]).astype(int)
-
-                    k = 0
-                    for i in range(num_patches_height):
-                        for j in range(num_patches_width):
-
-                            top = i * crop_size[0]
-                            left = j * crop_size[1]
-
-                            # Avoiding the patch to be outside the image
-                            if top + crop_size[0] >= height:
-                                top = height - crop_size[0]
-                            if left + crop_size[1] >= width:
-                                left = width - crop_size[1]
-
-                            output_image[
-                                :, top : top + crop_size[0], left : left + crop_size[1]
-                            ] = output_queue_patches[k]
-                            label_image[:, top : top + crop_size[0], left : left + crop_size[1]] = label_queue_patches[
-                                k
-                            ]
-                            k += 1
-
-                    # Deleting used patches
-                    label_queue_patches = label_queue_patches[k:]
-                    output_queue_patches = output_queue_patches[k:]
-
-                    # Extracting metrics
-                    num_pixels += output_image.flatten().shape[0]
-                    mse += torch.sum((output_image - label_image) ** 2)
-                    tp += torch.count_nonzero(
-                        torch.logical_and(output_image.flatten() >= 0.5, label_image.flatten() == 1)
-                    ).item()
-                    # tn += torch.count_nonzero(
-                    #     torch.logical_and(output_image.flatten() < 0.5, label_image.flatten() == 0)
-                    # ).item()
-                    fp += torch.count_nonzero(
-                        torch.logical_and(output_image.flatten() >= 0.5, label_image.flatten() == 0)
-                    ).item()
-                    fn += torch.count_nonzero(
-                        torch.logical_and(output_image.flatten() < 0.5, label_image.flatten() == 1)
-                    ).item()
+        # Extracting metrics
+        num_pixels += output_image.flatten().shape[0]
+        mse += torch.sum((output_image - label) ** 2)
+        tp += torch.count_nonzero(torch.logical_and(output_image.flatten() >= 0.5, label.flatten() == 1)).item()
+        fp += torch.count_nonzero(torch.logical_and(output_image.flatten() >= 0.5, label.flatten() == 0)).item()
+        fn += torch.count_nonzero(torch.logical_and(output_image.flatten() < 0.5, label.flatten() == 1)).item()
 
     # Calculating MSE
     mse = mse.item() / num_pixels
