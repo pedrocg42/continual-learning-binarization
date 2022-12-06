6d90a17dc72704ed527e6fffff4b79b3693a02db
pedrocg42 <pedrocorglez@gmail.com>
2022-12-06 05:53:17 +0100

    feat: add more config values

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 import os
 
-dataset_path = "G:/Pedro/Images/Binarization/"
+seed = 42
+
+device = "cpu"
+
+dataset_path = "data/"
+cross_val_splits = 5
 
 models_path = "results/models"
 os.makedirs(models_path, exist_ok=True)
 
 logs_path = "results/logs"
 os.makedirs(logs_path, exist_ok=True)
+
+ALL_DATASETS = [["PHI"], ["Salzinnes"], ["Dibco"], ["Einsieldeln"]]  # ["Palm"]
