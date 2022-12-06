c0306c187538d2dc0c6e06fb8e9c26d0f0c19b56
pedrocg42 <pedrocorglez@gmail.com>
2022-12-06 05:54:32 +0100

    feat: adding scenarios to experiment

--- experiments.py
+++ experiments.py
@@ -21,7 +21,7 @@ from data import BinDataset
 
 scenario_1 = [["PHI"], ["Salzinnes"], ["Dibco"], ["Einsieldeln"]]
 scenario_2 = [["Einsieldeln"], ["Dibco"], ["Salzinnes"], ["PHI"]]
-scenario_4 = [["PHI"], ["Dibco"], ["Einsieldeln"], ["Salzinnes"]]
+scenario_3 = [["PHI"], ["Dibco"], ["Einsieldeln"], ["Salzinnes"]]
 scenario_4 = [["Salzinnes"], ["Einsieldeln"], ["Dibco"], ["PHI"]]
 
 #################################################################
@@ -45,10 +45,10 @@ baseline_scenario_1 = {
     "criteria": MSELoss,
     "optimizer": Adam,
     "learning_rate": 3e-4,
-    "num_epochs": 1000,
-    "batch_size": 32,
-    "steps_per_epoch": 50,
-    "patience": 10,
+    "num_epochs": 2,
+    "batch_size": 4,
+    "steps_per_epoch": 2,
+    "patience": 25,
     "patience_learning_rate": 5,
 }
 
@@ -60,8 +60,8 @@ vq_scenario_1.update(
         "architecture_type": "vector_quantizer",
         "codebook_size": 8192,
         "num_codebooks": 1,
-        "vq_decay": 0.99,
-        "threshold_ema_dead_code": 10,
+        "vq_decay": 0.9,
+        "threshold_ema_dead_code": 1024,
         "num_epochs_initialization_keys": 10,
     }
 )
@@ -101,8 +101,8 @@ dkvb_scenario_1.update(
         "architecture_type": "discrete_key_value_bottleneck",
         "codebook_size": 8192,
         "num_codebooks": 1,
-        "dkvb_decay": 0.99,
-        "threshold_ema_dead_code": 10,
+        "vq_decay": 0.9,
+        "threshold_ema_dead_code": 1024,
         "num_epochs_initialization_keys": 10,
     }
 )
@@ -135,6 +135,196 @@ dkvb_scenario_1_512.update(
 )
 
 #################################################################
+################            SCENARIO 2              #############
+#################################################################
+
+baseline_scenario_2 = baseline_scenario_1.copy()
+baseline_scenario_2.update(
+    {
+        "name": "baseline_scenario_2",
+        "datasets": scenario_2,
+    }
+)
+
+# Baselines Vector Quantizer Experiments
+vq_scenario_2 = baseline_scenario_2.copy()
+vq_scenario_2.update(
+    {
+        "name": "vq_scenario_2",
+        "architecture_type": "vector_quantizer",
+        "codebook_size": 8192,
+        "num_codebooks": 1,
+        "vq_decay": 0.9,
+        "threshold_ema_dead_code": 1024,
+        "num_epochs_initialization_keys": 10,
+    }
+)
+
+vq_scenario_2_64 = vq_scenario_2.copy()
+vq_scenario_2_64.update(
+    {
+        "name": "vq_scenario_2_64",
+        "codebook_size": 128,
+        "num_codebooks": 64,
+    }
+)
+
+vq_scenario_2_128 = vq_scenario_2.copy()
+vq_scenario_2_128.update(
+    {
+        "name": "vq_scenario_2_128",
+        "codebook_size": 64,
+        "num_codebooks": 128,
+    }
+)
+
+vq_scenario_2_512 = vq_scenario_2.copy()
+vq_scenario_2_512.update(
+    {
+        "name": "vq_scenario_2_512",
+        "codebook_size": 16,
+        "num_codebooks": 512,
+    }
+)
+
+# Baselines Discrete Key-Value Bottleneck Experiments
+dkvb_scenario_2 = baseline_scenario_2.copy()
+dkvb_scenario_2.update(
+    {
+        "name": "dkvb_scenario_2",
+        "architecture_type": "discrete_key_value_bottleneck",
+        "codebook_size": 8192,
+        "num_codebooks": 1,
+        "vq_decay": 0.9,
+        "threshold_ema_dead_code": 1024,
+        "num_epochs_initialization_keys": 10,
+    }
+)
+
+dkvb_scenario_2_64 = dkvb_scenario_2.copy()
+dkvb_scenario_2_64.update(
+    {
+        "name": "dkvb_scenario_2_64",
+        "codebook_size": 128,
+        "num_codebooks": 64,
+    }
+)
+
+dkvb_scenario_2_128 = dkvb_scenario_2.copy()
+dkvb_scenario_2_128.update(
+    {
+        "name": "dkvb_scenario_2_128",
+        "codebook_size": 64,
+        "num_codebooks": 128,
+    }
+)
+
+dkvb_scenario_2_512 = dkvb_scenario_2.copy()
+dkvb_scenario_2_512.update(
+    {
+        "name": "dkvb_scenario_1_512",
+        "codebook_size": 16,
+        "num_codebooks": 512,
+    }
+)
+
+
+#################################################################
+################            SCENARIO 3              #############
+#################################################################
+
+baseline_scenario_3 = baseline_scenario_1.copy()
+baseline_scenario_3.update(
+    {
+        "name": "baseline_scenario_3",
+        "datasets": scenario_3,
+    }
+)
+
+# Baselines Vector Quantizer Experiments
+vq_scenario_3 = baseline_scenario_3.copy()
+vq_scenario_3.update(
+    {
+        "name": "vq_scenario_3",
+        "architecture_type": "vector_quantizer",
+        "codebook_size": 8192,
+        "num_codebooks": 1,
+        "vq_decay": 0.9,
+        "threshold_ema_dead_code": 1024,
+        "num_epochs_initialization_keys": 10,
+    }
+)
+
+vq_scenario_3_64 = vq_scenario_3.copy()
+vq_scenario_3_64.update(
+    {
+        "name": "vq_scenario_3_64",
+        "codebook_size": 128,
+        "num_codebooks": 64,
+    }
+)
+
+vq_scenario_3_128 = vq_scenario_3.copy()
+vq_scenario_3_128.update(
+    {
+        "name": "vq_scenario_3_128",
+        "codebook_size": 64,
+        "num_codebooks": 128,
+    }
+)
+
+vq_scenario_3_512 = vq_scenario_3.copy()
+vq_scenario_3_512.update(
+    {
+        "name": "vq_scenario_3_512",
+        "codebook_size": 16,
+        "num_codebooks": 512,
+    }
+)
+
+# Baselines Discrete Key-Value Bottleneck Experiments
+dkvb_scenario_3 = baseline_scenario_3.copy()
+dkvb_scenario_3.update(
+    {
+        "name": "dkvb_scenario_3",
+        "architecture_type": "discrete_key_value_bottleneck",
+        "codebook_size": 8192,
+        "num_codebooks": 1,
+        "vq_decay": 0.9,
+        "threshold_ema_dead_code": 1024,
+        "num_epochs_initialization_keys": 10,
+    }
+)
+
+dkvb_scenario_3_64 = dkvb_scenario_3.copy()
+dkvb_scenario_3_64.update(
+    {
+        "name": "dkvb_scenario_3_64",
+        "codebook_size": 128,
+        "num_codebooks": 64,
+    }
+)
+
+dkvb_scenario_3_128 = dkvb_scenario_3.copy()
+dkvb_scenario_3_128.update(
+    {
+        "name": "dkvb_scenario_3_128",
+        "codebook_size": 64,
+        "num_codebooks": 128,
+    }
+)
+
+dkvb_scenario_3_512 = dkvb_scenario_3.copy()
+dkvb_scenario_3_512.update(
+    {
+        "name": "dkvb_scenario_3_512",
+        "codebook_size": 16,
+        "num_codebooks": 512,
+    }
+)
+
+
+#################################################################
 ################            SCENARIO 4              #############
 #################################################################
 
@@ -154,8 +344,8 @@ vq_scenario_4.update(
         "architecture_type": "vector_quantizer",
         "codebook_size": 8192,
         "num_codebooks": 1,
-        "vq_decay": 0.99,
-        "threshold_ema_dead_code": 10,
+        "vq_decay": 0.9,
+        "threshold_ema_dead_code": 1024,
         "num_epochs_initialization_keys": 10,
     }
 )
@@ -195,8 +385,8 @@ dkvb_scenario_4.update(
         "architecture_type": "discrete_key_value_bottleneck",
         "codebook_size": 8192,
         "num_codebooks": 1,
-        "dkvb_decay": 0.99,
-        "threshold_ema_dead_code": 10,
+        "vq_decay": 0.9,
+        "threshold_ema_dead_code": 1024,
         "num_epochs_initialization_keys": 10,
     }
 )
