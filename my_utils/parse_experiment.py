d4ae2fbfa50af706d67d02609ee900e26e115226
pedrocg42 <pedrocorglez@gmail.com>
2022-12-06 05:57:11 +0100

    chore: formatting parse_experiment

--- my_utils/parse_experiment.py
+++ my_utils/parse_experiment.py
@@ -1,10 +1,11 @@
 import experiments
 
+
 def parse_experiment(function):
     def parser(*args, **kwargs):
-        
+
         experiment = getattr(experiments, kwargs["experiment"])
-        
+
         return function(*args, **experiment)
-    
+
     return parser
